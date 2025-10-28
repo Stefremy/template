0aab92efe193f21e6f75afe002bc8da391187a75
stef <stefano.remy@gmail.com>
2025-10-28 17:58:09 +0000

    b4 py

---
+++ scripts/fix_data_settings.ts
@@ -0,0 +1,4 @@
+// TypeScript version of fix_data_settings.py
+// TODO: Implement logic to fix data settings as in the Python script
+
+console.log('fix_data_settings.ts placeholder: implement logic here');
---
+++ scripts/repair_index.ts
@@ -0,0 +1,4 @@
+// TypeScript version of repair_index.py
+// TODO: Implement logic to repair index.html as in the Python script
+
+console.log('repair_index.ts placeholder: implement logic here');
